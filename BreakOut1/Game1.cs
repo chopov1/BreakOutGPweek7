db14a8cef2b08382eb12e0e927af0f795b2d99a9
Mack Pearson <76492881+chopov1@users.noreply.github.com>
2022-10-24 14:48:54 -0500

    vs commit 3

--- BreakOut1/Game1.cs
+++ BreakOut1/Game1.cs
@@ -19,7 +19,7 @@ namespace BreakOut1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            //cherries in the lab
             base.Initialize();
         }
 
